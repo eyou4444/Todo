9d7c8c8f5d4ee17ca0ab876b6c3afafe7b9d8e66
eyou4444 <eyou4444@163.com>
2017-08-25 00:08:49 +0800

    ceshi

--- fabfile.py
+++ fabfile.py
@@ -1,5 +1,5 @@
 from fabric.api import  *
-env.hosts=['47.94.130.184/']
+env.hosts=['47.94.130.184']
 env.user='root'
 env.password='huxin@860404'
 
@@ -7,7 +7,7 @@ def hello():
     print 'hello world!'
 
 def deploy():
-    with cd('/root/Todo')
+    with cd('/root/Todo'):
         run('git pull')
         sudo('supervisorctl restart todo')
         sudo('supervisorctl status')
--- run.py
+++ run.py
@@ -3,9 +3,12 @@ from flask import Flask
 app = Flask(__name__)
 
 
+
+
+
 @app.route('/')
 def hello_world():
-    return 'hello world~by zx!!!!!!!!!hahah'
+    return 'hello world~by zx!!这是一个自动化部署的程序！'
 
 
 if __name__ == '__main__':
