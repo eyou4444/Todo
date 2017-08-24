6fe4e8c20cbdb79cd26b48b553d06233403ceb18
eyou4444 <eyou4444@163.com>
2017-08-24 22:12:19 +0800

    hahah
    hahah

---
+++ fabfile.py
@@ -0,0 +1,13 @@
+from fabric.api import  *
+env.hosts=['47.94.130.184/']
+env.user='root'
+env.password='huxin@860404'
+
+def hello():
+    print 'hello world!'
+
+def deploy():
+    with cd('/root/Todo')
+        run('git pull')
+        sudo('supervisorctl restart todo')
+        sudo('supervisorctl status')
