7fef60333b4f1eabe0582259fce09d6aab61f426
eyou4444 <eyou4444@163.com>
2017-08-25 22:40:03 +0800

    8-25 22:39

--- run.py
+++ run.py
@@ -1,12 +1,20 @@
-from flask import Flask
+from flask import Flask,render_template
+
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    return '<h1>Hello world!!!!<h1>'
+    return render_template('index.html',title='Welcome!')
 
+@app.route('/services')
+def services():
+    return 'Service'
+
+@app.route('/about')
+def about():
+    return 'About'
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
