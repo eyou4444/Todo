f3c639c8a36283ad49455650792a9a510919b43a
eyou4444 <eyou4444@163.com>
2017-09-03 00:55:07 +0800

    unit 13

---
+++ app/__init__.py
@@ -0,0 +1,51 @@
+# -*- coding:utf-8 -*-
+from os import path  # 上传文件是指定路径。服务器地址
+
+from flask import Flask
+from flask_bootstrap import Bootstrap
+from flask_nav import Nav
+from flask_nav.elements import *
+from flask_sqlalchemy import SQLAlchemy
+from .views import init_views
+from werkzeug.routing import BaseConverter  # 德文写的正则模块包
+
+
+class RegexConverter(BaseConverter):  # 正则表达式转换器
+    def __init__(self, url_map, *items):
+        super(RegexConverter, self).__init__(url_map)
+        self.regex = items[0]
+
+
+basedir = path.abspath(path.dirname(__file__))  # 配置绝对路径，当前的项目路径
+
+bootstrap = Bootstrap()  # 这个变量是全局变量，引用这个文件的时候可以使用
+nav = Nav()
+db = SQLAlchemy()
+
+
+#使用工厂方法创建app
+def create_app():
+    app = Flask(__name__)
+    app.url_map.converters['regex'] = RegexConverter
+    # 初始化时把他初始化到url_map中，取名字叫regex
+    app.config.from_pyfile('config')
+    # 配置数据库连接
+    app.config['SQLALCHEMY_DETABASE_URI'] = \
+        'sqlite:///' + path.join(basedir, 'data.sqlite')
+    app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+
+    nav.register_element('top', Navbar(u'光荣之路',
+                                       View(u'主页', 'index'),
+                                       View(u'关于', 'about'),
+                                       View(u'服务', 'services'),
+                                       View(u'程序', 'projects'),
+                                       View(u'登录', 'login')))
+    db.init_app(app) #将插件进行初始化
+    bootstrap.init_app(app)
+    nav.init_app(app)
+    init_views(app)
+    return app
+
+
+
+
---
+++ manager.py
@@ -0,0 +1,29 @@
+# -*- coding:utf-8 -*-
+from flask_script import Manager
+from app import create_app
+app = create_app()
+manager = Manager(app)
+
+
+@manager.command
+def dev():
+    from livereload import Server
+    live_server = Server(app.wsgi_app)
+    live_server.watch('**/*.*')  # 指明一个需要监测的目录,表示监测所有目录
+    #自己打开浏览器
+    live_server.serve(open_url=True)
+
+#测试使用
+@manager.command
+def test():
+    pass
+
+#第一次安装程序是使用，安装环境或者数据库等
+@manager.command
+def deploy():
+    pass
+
+# 主要的引导文件。
+if __name__ == '__main__':
+    manager.run()
+
